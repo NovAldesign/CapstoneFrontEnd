5a9a9de792958fc6662b04faf71f4be5bfc8b7e7
NovAldesign <williams.lavon@gmail.com>
2026-05-28 16:05:40 -0400

    updated main.jsx to include CartContext.jsx import

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async"; // Import the provider
+import { CartProvider } from "./Context/CartContext.jsx"; // 1. Added our brand new Cart Context
 import App from "./App.jsx";
 import "./Styles/Index.css";
 
@@ -9,7 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider> {/* Wrap the app to enable dynamic meta tags */}
       <BrowserRouter>
-        <App />
+        <CartProvider> {/* 2. Wrapped App so every page can see the bundle cart */}
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>,
